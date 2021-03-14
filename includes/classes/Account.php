e0b5021e5d4f3ddf1e30b9dadb38fd5711659a9e
SeungJae <stopj1123@gmail.com>
2021-03-14 22:51:30 +0900

    Make Validate Function for register page

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -13,10 +13,23 @@
             $this->validateLastname($ln);
             $this->validateEmails($em, $em2);
             $this->validatePasswords($pw, $pw2);
+
+            if(empty($this->errorArray) == true) {
+                // Insert into db
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public function getError($error) {
+            if(!in_array($error, $this->errorArray)) {
+                $error = "";
+            }
+            return "<span class='errorMessage'>$error</span>";
         }
 
         private function validateUsername($un) {
-            
             if(strlen($un) > 25 || strlen($un) < 5) {
                 array_push($this->errorArray, "Your username must be between 5 and 25 characters");
                 return;
@@ -26,19 +39,49 @@
         }
         
         private function validateFirstname($fn) {
-            
+            if(strlen($fn) > 25 || strlen($fn) < 2) {
+                array_push($this->errorArray, "Your Firstname must be between 2 and 25 characters");
+                return;
+            }
         }
         
         private function validateLastname($ln) {
-            
+            if(strlen($ln) > 25 || strlen($ln) < 2) {
+                array_push($this->errorArray, "Your Lastname must be between 2 and 25 characters");
+                return;
+            }
         }
         
         private function validateEmails($em, $em2) {
-            
+            if($em != $em2) {
+                array_push($this->errorArray, "Your emails don't match");
+                return;
+            }
+
+            if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {               // Validates whether the value is a valid e-mail address
+                array_push($this->errorArray, "Email is invalid");
+                return;
+            }
+
+            // TODO::Check that username hasn't already been used
         }
         
         private function validatePasswords($pw, $pw2) {
             
+            if($pw != $pw2) {
+                array_push($this->errorArray, "Your passwords don't match");
+                return;
+            }
+
+            if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+                array_push($this->errorArray, "Your password can only contain numbers and letters");
+                return;
+            }
+
+            if(strlen($pw) > 30 || strlen($pw) < 5) {
+                array_push($this->errorArray, "Your password must be between 5 and 30 characters");
+                return;
+            }
         }
 
     }
