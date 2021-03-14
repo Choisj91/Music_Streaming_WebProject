5c85d49d4539684885a4aeffc391e1886b67876e
SeungJae <stopj1123@gmail.com>
2021-03-14 14:00:47 +0900

    Create register-handler and Class files for Account

---
+++ includes/classes/Account.php
@@ -0,0 +1,45 @@
+<?php
+    class Account {
+
+        private $errorArray;
+
+        public function __construct() {
+            $this->errorArray = array();
+        }
+
+        public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
+            $this->validateUsername($un);
+            $this->validateFirstname($fn);
+            $this->validateLastname($ln);
+            $this->validateEmails($em, $em2);
+            $this->validatePasswords($pw, $pw2);
+        }
+
+        private function validateUsername($un) {
+            
+            if(strlen($un) > 25 || strlen($un) < 5) {
+                array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+                return;
+            }
+
+            // TODO::check if username exists
+        }
+        
+        private function validateFirstname($fn) {
+            
+        }
+        
+        private function validateLastname($ln) {
+            
+        }
+        
+        private function validateEmails($em, $em2) {
+            
+        }
+        
+        private function validatePasswords($pw, $pw2) {
+            
+        }
+
+    }
+?>
