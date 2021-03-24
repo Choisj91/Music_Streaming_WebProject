a738c43db24e8cc996aa87ec3198cfbb3418626d
SeungJae <stopj1123@gmail.com>
2021-03-25 00:09:00 +0900

    Add jQuery to handle changing page

--- register.php
+++ register.php
@@ -21,6 +21,8 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="assets/js/register.js"></script>
 </head>
 <body>
 	<div id="background">
