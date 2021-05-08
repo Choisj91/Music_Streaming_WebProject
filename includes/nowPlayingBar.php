cf8095a3618ab074d87396534a62650f4a6bd1ca
SeungJae <stopj1123@gmail.com>
2021-05-08 16:07:29 +0900

    Make Repeat function of music playere

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -92,6 +92,12 @@ function nextSong() {
 	setTrack(trackToPlay, currentPlaylist, true);
 }
 
+function setRepeat() {
+	repeat = !repeat;
+	var imageName = repeat ? "repeat-active.png" : "repeat.png";
+	$(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);	
+}
+
 function setTrack(trackId, newPlaylist, play) {
 
 	currentIndex = currentPlaylist.indexOf(trackId);
@@ -196,7 +202,7 @@ function pauseSong() {
 						<img src="assets/images/icons/next.png" alt="Next">
 					</button>
 
-					<button class="controlButton repeat" title="Repeat button">
+					<button class="controlButton repeat" title="Repeat button" onclick="setRepeat()">
 						<img src="assets/images/icons/repeat.png" alt="Repeat">
 					</button>
 
