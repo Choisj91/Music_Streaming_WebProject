4a4e5d140b10605b632f61f490870d1997d3b4d2
SeungJae <stopj1123@gmail.com>
2021-05-09 12:01:16 +0900

    Create browse.php and fix index.php

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -177,11 +177,12 @@ function setTrack(trackId, newPlaylist, play) {
 		});
 
 		audioElement.setTrack(track);
+
+		if(play == true) {
+			playSong();
+		}
 	});
 
-	if(play == true) {
-		audioElement.play();
-	}
 }
 
 function playSong() {
