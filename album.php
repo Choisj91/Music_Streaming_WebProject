da3771260b5f5a2c56dc9cab70243e38635b9300
SeungJae <stopj1123@gmail.com>
2021-05-15 15:49:12 +0900

    Create the header section of the playlist page

--- album.php
+++ album.php
@@ -19,7 +19,7 @@ $artist = $album->getArtist();
 
 	<div class="rightSection">
 		<h2><?php echo $album->getTitle(); ?></h2>
-		<p>By <?php echo $artist->getName(); ?></p>
+		<p role="link" tabindex="0" onclick="openPage('artist.php?id=$artistId')">By <?php echo $artist->getName(); ?></p>
 		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
 
 	</div>
