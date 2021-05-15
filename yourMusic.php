3bc8a637c3eb6d684dfd97285d630fba3ba6086b
SeungJae <stopj1123@gmail.com>
2021-05-15 14:36:33 +0900

    Create playlist.php class

--- yourMusic.php
+++ yourMusic.php
@@ -8,8 +8,36 @@ include("includes/includedFiles.php");
         <h2>PLAYLISTS</h2>
 
         <div class="buttonItems">
-            <button class="button green" onclick="createPlaylist('jae')">NEW PLAYLIST</button>
+            <button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
         </div>
+
+        <?php
+            $username = $userLoggedIn->getUsername();
+
+            $playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner = '$username'");
+
+		    if(mysqli_num_rows($playlistsQuery) == 0) {
+		    	echo "<span class='noResults'>No playlists found</span>";
+		    }
+
+            while($row = mysqli_fetch_array($playlistsQuery)) {
+
+                $playlist = new Playlist($con, $row);
+
+                echo "<div class='gridViewItem'>
+
+                        <div class='playlistImage'>
+                            <img src='assets/images/icons/playlist.php'>
+                        </div>
+                
+                        <div class='gridViewInfo'>"
+                            . $playlist->getName() .
+                        "</div>
+
+                    </div>";
+
+            }  
+        ?>
 
     </div>    
 
