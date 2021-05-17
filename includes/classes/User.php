fa47c268232d5be3efab7fb87fa07723e109e142
SeungJae <stopj1123@gmail.com>
2021-05-18 03:25:09 +0900

    Create Email Authen function

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -2,19 +2,25 @@
 	class User {
 
 		private $con;
-        private $username;
+		private $username;
 
 		public function __construct($con, $username) {
 			$this->con = $con;
 			$this->username = $username;
 		}
 
-        public function getUsername() {
-            return $this->username;
-        }
+		public function getUsername() {
+			return $this->username;
+		}
+
+		public function getEmail() {
+			$query = mysqli_query($this->con, "SELECT email FROM users WHERE username='$this->username'");
+			$row = mysqli_fetch_array($query);
+			return $row['email'];
+		}
 
 		public function getFirstAndLastName() {
-			$query = mysqli_query($this->con, "SELECT concat(firstName, ' ', lastName) as 'name' FROM users WHERE username='$this->username'");
+			$query = mysqli_query($this->con, "SELECT concat(firstName, ' ', lastName) as 'name'  FROM users WHERE username='$this->username'");
 			$row = mysqli_fetch_array($query);
 			return $row['name'];
 		}
